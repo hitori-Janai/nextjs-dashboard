c062c1113962b325e9ed99bfed1584c03a617b3b
sakkura618 <sakkura618@gmail.com>
2025-05-04 09:01:28 +0800

    ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  eslint: {
+    // Allow production builds to complete even if ESLint errors are present
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
